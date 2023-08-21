ff50fd70d4834f20bd14e48a5186a213f0f1ca63
htk96 <49614453+htk96@users.noreply.github.com>
2023-08-21 10:00:12 +0900

    Merge Templat

---
+++ words/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from words import views
+
+urlpatterns = [
+    path("", views.WordList.as_view(), name="words"),
+    path("word-input", views.WordInput.as_view(), name="word-input"),
+    path("word-save", views.WordSave.as_view(), name="word-save"),
+    path("word/<int:word_id>/", views.WordEdit.as_view(), name="word-edit"),
+]
